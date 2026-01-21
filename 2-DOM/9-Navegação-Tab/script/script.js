3df9eb53d8791b6ccceb02d9944b05f89a869e78
Igor Calandrine <igorwow@hotmail.com>
2026-01-21 00:10:08 -0300

    Animação na Rolagem

--- 2-DOM/9-Navegação-Tab/script/script.js
+++ 2-DOM/9-Navegação-Tab/script/script.js
@@ -18,7 +18,6 @@ function animaisTexto () {
          })
       }) 
    }
-
 }
 
 function perguntasFaqc() {
@@ -30,36 +29,72 @@ function perguntasFaqc() {
       pergunta.addEventListener("click", () => {
          pergunta.classList.toggle("js-desativa-p")
          pergunta.nextElementSibling.classList.toggle("js-resposta-desativa")
-
       })
    })
    perguntasFaq[0].nextElementSibling.classList.remove("js-resposta-desativa")
    perguntasFaq[0].classList.add("js-desativa-p")
-
 }
 
-function scrollTitulo () {
+function scrollTitulo () { //Por ordem
    const linkInterno = document.querySelectorAll("a[href^='#']")
    const tituloInternos = document.querySelectorAll(".js-scroll")
 
    if (linkInterno.length) {
-      
       linkInterno.forEach((item, index) => {
          
          item.addEventListener("click", (event) => {
             event.preventDefault()
-
             const localScroll = {top: tituloInternos[index].offsetTop, behavior: "smooth"}
-            
             window.scroll(localScroll)
          })
       })
    }
 }
 
+function scrollInternoSuave () { //Por href===id
+   const linkInterno = document.querySelectorAll("a[href^='#']")
+
+   linkInterno.forEach(item => {
+      
+      item.addEventListener("click", (event) => {
+         const linkInterno = event.target.getAttribute("href")
+         const tituloInterno = document.querySelector(linkInterno)
+         event.preventDefault()
+         
+         const scrollTo = {top: tituloInterno.offsetTop, behavior: "smooth"}
+         window.scroll(scrollTo)
+      })
+   })   
+}
+
+function TituloAnimadoScroll() {
+   const alturaTela = window.innerHeight
+   const tituloAnima = document.querySelectorAll(".js-scrollAnima")
+
+   tituloAnima.forEach((item) => {
+      item.classList.add("js-scrollAnimação")
+   })
+   tituloAnima[0].classList.remove("js-scrollAnimação")
+
+   window.addEventListener("scroll", () => {
+
+      tituloAnima.forEach((item) => {
+         
+         if (item.getBoundingClientRect().bottom > 0 && item.getBoundingClientRect().top < alturaTela*0.85) {
+            item.classList.remove("js-scrollAnimação")
+         }
+         else if (item.getBoundingClientRect().bottom < 0 || item.getBoundingClientRect().top > alturaTela*0.7) {
+            item.classList.add("js-scrollAnimação")
+         }
+      })
+   })
+}
+
+// scrollTitulo ()
 animaisTexto()
 perguntasFaqc()
-scrollTitulo ()
+scrollInternoSuave()
+TituloAnimadoScroll()
 
 
 
